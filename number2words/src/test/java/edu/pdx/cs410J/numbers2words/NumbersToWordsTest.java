93e8ecf436dd49794a8a965a1611ed19b804f301
David Whitlock <david.m.whitlock@gmail.com>
2017-07-14 20:56:14 -0700

    Remove remaining vestiges of the airline application.

--- number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
+++ number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
@@ -1,31 +1,8 @@
 package edu.pdx.cs410J.numbers2words;
 
-import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 /**
  * Unit tests for the {@link NumbersToWords} class.
  */
 public class NumbersToWordsTest {
-  
-  @Test(expected = UnsupportedOperationException.class)
-  public void getArrivalStringNeedsToBeImplemented() {
-    NumbersToWords flight = new NumbersToWords();
-    flight.getArrivalString();
-  }
-
-  @Test
-  public void initiallyAllFlightsHaveTheSameNumber() {
-    NumbersToWords flight = new NumbersToWords();
-    assertThat(flight.getNumber(), equalTo(42));
-  }
-
-  @Test
-  public void forProject1ItIsOkayIfGetDepartureTimeReturnsNull() {
-    NumbersToWords flight = new NumbersToWords();
-    assertThat(flight.getDeparture(), is(nullValue()));
-  }
   
 }

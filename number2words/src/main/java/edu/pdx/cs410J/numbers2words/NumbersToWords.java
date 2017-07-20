2b42b2c7221d9c48babc0f2db99f5a8c3532641b
patter5 <jeffreypine@gmail.com>
2017-07-19 19:18:41 -0700

    worked on parse test for NTW

--- number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWords.java
+++ number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWords.java
@@ -2,4 +2,8 @@ package edu.pdx.cs410J.numbers2words;
 
 public class NumbersToWords {
 
+    public String parse(String input) throws IllegalArgumentException {
+        throw new IllegalArgumentException("Bad argument: " + input);
+        //return "";
+    }
 }
--- number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
+++ number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
@@ -1,8 +1,19 @@
 package edu.pdx.cs410J.numbers2words;
 
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  * Unit tests for the {@link NumbersToWords} class.
  */
 public class NumbersToWordsTest {
+
+    @Test(expected = IllegalArgumentException.class)
+    public void doesNotSupportNonNumbers() {
+        NumbersToWords ntw = new NumbersToWords();
+
+        String word = ntw.parse("chicken");
+    }
   
 }

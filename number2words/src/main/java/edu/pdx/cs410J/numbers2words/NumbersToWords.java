24c8ed800b00ccab20960d0245a5cf3958c11f53
andyturnerpdx <andy.turner@pdx.edu>
2017-07-19 19:36:44 -0700

    Andy Turner and Anthony Tran, parse "1"...

--- number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWords.java
+++ number2words/src/main/java/edu/pdx/cs410J/numbers2words/NumbersToWords.java
@@ -3,7 +3,10 @@ package edu.pdx.cs410J.numbers2words;
 public class NumbersToWords {
 
     public String parse(String input) throws IllegalArgumentException {
+
+        if(input.equals("1")) {
+            return "One Dollar";
+        }
         throw new IllegalArgumentException("Bad argument: " + input);
-        //return "";
     }
 }
--- number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
+++ number2words/src/test/java/edu/pdx/cs410J/numbers2words/NumbersToWordsTest.java
@@ -2,6 +2,7 @@ package edu.pdx.cs410J.numbers2words;
 
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
@@ -12,8 +13,13 @@ public class NumbersToWordsTest {
     @Test(expected = IllegalArgumentException.class)
     public void doesNotSupportNonNumbers() {
         NumbersToWords ntw = new NumbersToWords();
+        ntw.parse("chicken");
+    }
 
-        String word = ntw.parse("chicken");
+    @Test
+    public void testSingleStringToNumber(){
+        NumbersToWords ntw = new NumbersToWords();
+        assertThat(ntw.parse("1"), equalTo("One Dollar"));
     }
   
 }
